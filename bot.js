32d77a3da7365b9164d3da06af3be45eb5c955ce
Jiwanshu Verma <jiwanshu2003@gmail.com>
2018-12-31 13:56:37 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,45 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !truth
+            case 'Truth':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'What is the most wholesome thing you have ever done'
+                });
+            break;
+            // !dare
+	    case 'Dare':
+		bot.sendMessage({
+		    to: channelID,
+		    message: 'I dare you to yell yeet at the top of your voice'
+		});
+	    break;	
+         }
+     }
+});
